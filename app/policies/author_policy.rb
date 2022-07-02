e7a362285fffd3ed95ecd10d0828bb6ecc727a00
matsumoto-minami-26 <matsumoto@p-partners.co.jp>
2022-07-02 18:24:46 +0900

    fix_policy_settings

--- app/policies/author_policy.rb
+++ app/policies/author_policy.rb
@@ -1,2 +1,19 @@
 class AuthorPolicy < TaxonomyPolicy
+  
+  def index?
+    # アクセスユーザー権限がadminまたはeditorのときのみ認可
+    user.admin? || user.editor?
+  end
+
+  def edit?
+    user.admin? || user.editor?
+  end
+
+  def update?
+    user.admin? || user.editor?
+  end
+
+  def destroy?
+    user.admin? || user.editor?
+  end
 end
