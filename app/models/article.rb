531e351b56b9bab4201ef03b26a23e25039d6357
matsumoto-minami-26 <matsumoto@p-partners.co.jp>
2022-07-02 17:42:50 +0900

    fix_article

--- app/models/article.rb
+++ app/models/article.rb
@@ -65,7 +65,7 @@ class Article < ApplicationRecord
   scope :new_arrivals, -> { viewable.order(published_at: :desc) }
   scope :by_category, ->(category_id) { where(category_id: category_id) }
   # scope :by_state, ->(state) { where(state: state) }
-  scope :by_tag, ->(tag_id) { joins(:tags).where(article_tags: {tag_id: tag_id}) }
+  scope :by_tag, ->(tag_id) { joins(:tags).where(article_tags: { tag_id: tag_id }) }
   scope :by_author, ->(author_id) { where(author_id: author_id) }
   scope :title_contain, ->(word) { where('title LIKE ?', "%#{word}%") }
   scope :body_contain, ->(word) { joins(:sentences).merge(where('sentences.body LIKE ?', "%#{word}%")) }
